e5d2b7d4a34f20dc2c9d9452b921a6476d0a226e
Hugo Silva <hugoengcomp@gmail.com>
2020-01-25 08:31:16 -0300

    Refatorando Callbacks para Promises

--- index.js
+++ index.js
@@ -1,33 +1,68 @@
 
 
-function obterUsuario(callback) {
-    setTimeout( function() {
-        return callback(null,{
-            id: 1,
-            nome: "Hugo",
-            dataNascimento: new Date()
-        })
-    }, 1000)
+
+//Refatorando o Callbacks para Promises
+function obterUsuario() {
+    /**
+     * Erroro => Reject
+     * Success => resolve
+     */
+    return new Promise(function resolvePromise( resolve, reject){
+        setTimeout( function() {
+            return resolve({
+                id: 1,
+                nome: "Hugo",
+                dataNascimento: new Date()
+            })
+        }, 1000)
+
+    })
 }
 
-function obterTelefone(idUsuario, callback) {
-    setTimeout(() => {
-        return callback(null,{
-            telefone: '54654564',
-            ddd: '71'
-        })
-    }, 2000)
+function obterTelefone(idUsuario) {
+    return new Promise(function resolvetelefone(resolve, reject){
+        setTimeout(() => {
+            return resolve({
+                telefone: '54654564',
+                ddd: '71'
+            })
+        }, 2000)
+
+    })
 }
 
 function obterEndereco(idUsuario, callback){
     setTimeout(() => {
         return callback(null, {
-            rua: 'Travessa Acalanto'
+            rua: 'Av. Presidente Dutra'
         })
     }, 2000)
 }
 
-
+const usuarioPromise = obterUsuario()
+//Para manipular o sucesso usamos a função .then
+//Manipular error .catch
+//Usuario --> telefone --> telefone
+usuarioPromise
+    .then(function (usuario){
+            return obterTelefone(usuario.id)
+            .then(function resolveTelefone (result){
+                return {
+                    usuario: {
+                        nome: usuario.nome,
+                        id: usuario.id
+                    },
+                    telefone: result
+                }
+            })
+    })
+    .then(function (resultado) {
+        console.log('Resultado', resultado)
+    })
+    .catch(function (error) {
+        console.log('Error', error)
+    })
+/*
  obterUsuario(function resolverUsuario(error, usuario) {    
     // null || "" || 0 === false 
     if (error) {
@@ -52,3 +87,4 @@ function obterEndereco(idUsuario, callback){
          })
      })     
  })
+*/
---
+++ readme.md
@@ -0,0 +1,5 @@
+#Ciclo de vida do Promises
+- Pedding: Estado inicial, ainda não terminou ou ainda não foi rejeitado.
+- FullFilled: Quando executou todas as operações com sucesso.
+- Refected: Quando a operação falhou
+Nota: A Promisse pode tornar uma código totalmente recursivo!
