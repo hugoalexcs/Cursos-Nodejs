cc39ee405207e7e1fc77d501a20b37c7c1126016
Hugo Silva <hugoengcomp@gmail.com>
2020-01-28 20:17:58 -0300

    Trabalhando com lista com JavaScript

---
+++ 04-lists/for-forin/services.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const URL = 'https://swapi.co/api/people';
+
+async function obterPessoas(nome) {
+    const url = `${URL}/?search=${nome}&format=json`;
+    const response = await axios.get(url);
+    return response.data;
+}
+
+obterPessoas('r2')
+    .then(function (resultado){
+        console.log('Resultado', resultado);
+    })
+    .catch(function (error) {
+        console.error('Error ', error);
+    })
